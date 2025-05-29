a03d8b0b8dbfaa5901ae0383b5ddc8171fddd9b0
Safwan Labbi <safwanlabbi@gmail.com>
2025-05-29 14:24:11 +0200

    removed old files

--- synthetic_env/utils.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def RandomSimplexVector(d=5, size=[1,] ):
-    vec = np.random.exponential(scale=1.0, size=size + [d,])
-    vec = vec / np.sum(vec, axis=-1).reshape(size + [1,])
-    return vec
